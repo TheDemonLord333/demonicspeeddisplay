ac9979d3d7d24a81077db758db7be7ba2886ba96
TheDemonLord333 <thedemonlord333@gmail.com>
2026-09-23 23:43:53 +0200

    Initial Commit

---
+++ demonicspeeeddisplayTests/demonicspeeeddisplayTests.swift
@@ -0,0 +1,17 @@
+//
+//  demonicspeeeddisplayTests.swift
+//  demonicspeeeddisplayTests
+//
+//  Created by David Martens on 23.09.26.
+//
+
+import Testing
+@testable import demonicspeeeddisplay
+
+struct demonicspeeeddisplayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
